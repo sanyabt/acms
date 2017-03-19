516f1eab6ea01f625271bece0a534989f02b5e25
Shreya Gupta <shreya07.gupta@gmail.com>
2017-03-20 00:20:43 +0530

    Update views.py

--- onboarding/views.py
+++ onboarding/views.py
@@ -107,6 +107,29 @@ def login_operational_user(request):
 
 
 
+
+
+def login_new_user(request):
+    if request.method == "POST":
+        mail = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=mail, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                #posts = Post.objects.all()
+                #return render(request, 'blog/post_list.html', {'posts': posts})
+            else:
+                return render(request, 'blog/login_new_user.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'blog/login_new_user.html', {'error_message': 'Invalid login'})
+    return render(request, 'blog/login_new_user.html')
+
+
+
+
+
+
 def logout_view(request):
     logout(request)
     posts = Table1.objects.all()
